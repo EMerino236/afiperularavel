ebf0602988f2da168a4c4629497a4cd2f1f209dc
luigilimaylla <luigi.limaylla@gmail.com>
2015-10-23 17:45:16 -0500

    Padrinos
    Busqueda de padrinos

--- app/controllers/PadrinosController.php
+++ app/controllers/PadrinosController.php
@@ -17,4 +17,70 @@ class PadrinosController extends BaseController
 			return View::make('error/error');
 		}
 	}
+
+	public function list_padrinos()
+	{
+		if(Auth::check()){
+			$data["inside_url"] = Config::get('app.inside_url');
+			$data["user"] = Session::get('user');
+			$data["permisos"] = Session::get('permisos');
+			if(in_array('side_listar_padrinos',$data["permisos"])){
+				$data["search"] = null;
+				$data["padrinos_data"] = Padrino::getPadrinosInfo()->paginate(10);
+				return View::make('padrinos/listPadrinos',$data);
+			}else{
+				return View::make('error/error');
+			}
+
+		}else{
+			return View::make('error/error');
+		}
+	}
+
+	public function search_padrino()
+	{
+		if(Auth::check()){
+			$data["inside_url"] = Config::get('app.inside_url');
+			$data["user"] = Session::get('user');
+			$data["permisos"] = Session::get('permisos');
+			if(in_array('side_listar_padrinos',$data["permisos"])){
+				$data["search"] = Input::get('search');
+				$data["padrinos_data"] = Padrino::searchPadrinos($data["search"])->paginate(10);
+				return View::make('padrinos/listPadrinos',$data);
+			}else{
+				return View::make('error/error');
+			}
+		}else{
+			return View::make('error/error');
+		}
+	}
+
+	public function render_edit_padrino($id=null)
+	{
+		if(Auth::check()){
+			$data["inside_url"] = Config::get('app.inside_url');
+			$data["user"] = Session::get('user');
+			$data["permisos"] = Session::get('permisos');
+			if((in_array('side_nuevo_padrino',$data["permisos"])) && $id){
+				$data["padrino_info"] = Padrino::searchPadrinoById($id)->get();
+				if($data["padrino_info"]->isEmpty()){
+					Session::flash('error', 'No se encontró al padrino.');
+					return Redirect::to('padrinos/list_padrinos');
+				}
+				$data["padrino_info"] = $data["padrino_info"][0];
+				/*$data["perfiles"] = User::getPerfilesPorUsuario($data["user_info"]->id)->get();*/
+				return View::make('padrinos/editPadrino',$data);
+			}else{
+				return View::make('error/error');
+			}
+		}else{
+			return View::make('error/error');
+		}
+	}
+
+	public function submit_edit_padrino()
+	{
+		return View::make('error/error');
+	}
+
 }
--- app/models/Padrino.php
+++ app/models/Padrino.php
@@ -8,4 +8,42 @@ class Padrino extends Eloquent{
 
 	protected $primaryKey = 'idpadrinos';
 
+	public function scopeGetPadrinosInfo($query)
+	{
+		$query->withTrashed()
+			  ->join('users','users.id','=','padrinos.idusers')
+			  ->join('personas','personas.idpersonas','=','users.idpersona')
+			  ->join('periodo_pagos','periodo_pagos.idperiodo_pagos','=','padrinos.idperiodo_pagos')
+			  ->select('personas.nombres','personas.apellido_pat','personas.apellido_mat','periodo_pagos.nombre','users.*');
+		return $query;
+	}
+
+	public function scopeSearchPadrinos($query,$search_criteria)
+	{
+		$query->withTrashed()
+			  ->join('users','users.id','=','padrinos.idusers')
+			  ->join('personas','personas.idpersonas','=','users.idpersona')
+			  ->join('periodo_pagos','periodo_pagos.idperiodo_pagos','=','padrinos.idperiodo_pagos')
+			  ->whereNested(function($query) use($search_criteria){
+			  		$query->where('users.num_documento','LIKE',"%$search_criteria%")
+			  			  ->orWhere('users.email','LIKE',"%$search_criteria%")
+			  			  ->orWhere('personas.nombres','LIKE',"%$search_criteria%")
+			  			  ->orWhere('personas.apellido_pat','LIKE',"%$search_criteria%")
+			  			  ->orWhere('personas.apellido_mat','LIKE',"%$search_criteria%");
+			  })
+			  ->select('personas.nombres','personas.apellido_pat','personas.apellido_mat','periodo_pagos.nombre','users.*');
+		return $query;
+	}
+
+	public function scopeSearchPadrinoById($query,$search_criteria)
+	{
+		$query->withTrashed()
+			  ->join('users','users.id','=','padrinos.idusers')
+			  ->join('personas','personas.idpersonas','=','users.idpersona')
+			  ->join('tipo_identificacion','tipo_identificacion.idtipo_identificacion','=','users.idtipo_identificacion')
+			  ->where('users.id','=',$search_criteria)
+			  ->select('tipo_identificacion.nombre as nombre_tipo_identificacion','personas.*','users.*');
+		return $query;
+	}
+
 }
