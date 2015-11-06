9a073e00c4b0fa6d881a04ae1f71104de3f5d6c6
luigilimaylla <luigi.limaylla@gmail.com>
2015-11-05 23:33:59 -0500

    Padrinos - Aprobacion
    20151105 11:33

--- app/views/padrinos/listPrepadrinos.blade.php
+++ app/views/padrinos/listPrepadrinos.blade.php
@@ -12,7 +12,8 @@
 			<th>DNI</th>
 			<th>Nombres</th>
 			<th>Apellido Paterno</th>
-			<th>Apellido Materno</th>			
+			<th>Apellido Materno</th>
+			<th>Aprobación <input type="checkbox" name="seleccionar-todos-aprobados" value="0"></th>
 		</tr>
 		@foreach($prepadrinos_data as $prepadrino_data)
 		<tr class="@if($prepadrino_data->deleted_at) bg-danger @endif">
@@ -28,9 +29,31 @@
 				</td>
 				<td>
 					{{$prepadrino_data->apellido_mat}}
+				</td>
+				<td class="text-center" style="vertical-align:middle">
+					<input type="checkbox" name="aprobacion" class="checkbox-aprobacion" value="0">
+					{{ Form::hidden('aprobaciones[]', $prepadrino_data->aprobacion,array('class'=>'hidden-aprobacion')) }}
 				</td>				
 			@endif
 		</tr>
 		@endforeach
+			
 	</table>
+	<div class="row">
+		<div class="form-group col-xs-8">
+			<span>*La contraseña será autogenerada y enviada al email ingresado.</span>
+		</div>
+	</div>
+	<div class="col-xs-12">
+		<div class="row">
+			<div class="form-group col-xs-8">	
+			@if(!$prepadrino_data->deleted_at)		
+				{{ Form::open(array('url'=>'padrinos/submit_aprove_prepadrino', 'role'=>'form')) }}
+				{{ Form::hidden('prepadrino_id', $prepadrino_data->idprepadrinos) }}
+				{{ Form::submit('Aprobar',array('prepadrino_id'=>'submit-delete', 'class'=>'btn btn-success')) }}							
+			@endif
+			{{ Form::close() }}
+			</div>
+		</div>
+	</div>
 @stop
