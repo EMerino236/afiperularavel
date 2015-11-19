cf5bcb84c9ba8fca831eff59513200f6485f5ad5
Diego Malpartida <diego200039@hotmail.com>
2015-11-19 14:07:56 -0500

    ws juego fix

--- app/controllers/api/juego/JuegoController.php
+++ app/controllers/api/juego/JuegoController.php
@@ -153,11 +153,14 @@ class JuegoController extends \BaseController {
         
         // verificar si ya se registro un puntaje para el jugador en el nivel
         $registro_puntaje = Puntaje::where('idPlayer', '=', $idJugador)->where('idLevel', '=', $idNivel)->first();
-        if($registro_puntaje && ($puntaje > $registro_puntaje->score))
+        if($registro_puntaje)
         {
-            // si ya existe un puntaje y es menor al nuevo puntaje, se modifica
-            $registro_puntaje->score = $puntaje;
-            $registro_puntaje->save();
+            if($puntaje > $registro_puntaje->score)
+            {
+                // si ya existe un puntaje y es menor al nuevo puntaje, se modifica
+                $registro_puntaje->score = $puntaje;
+                $registro_puntaje->save();
+            }
         }
         else
         {
