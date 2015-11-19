8060bfaab1ed1de62c1cc3644132b2ccbe78fca0
DeluxeAlonso <alonso.alvarez@pucp.pe>
2015-11-19 01:49:14 -0500

    Add notification script

---
+++ AfiPeruPush/afipush.php
@@ -0,0 +1,51 @@
+<?php
+
+// Put your device token here (without spaces):
+$deviceToken = '213cc9b0823ff6248e19d4b0f4c306dc844bb19854e822c28054fb06a5df40c6';
+
+// Put your private key's passphrase here:
+$passphrase = 'alonso3000';
+
+// Put your alert message here:
+$message = 'Ya no falta nada para la sesión del día 23 de noviembre!';
+
+////////////////////////////////////////////////////////////////////////////////
+
+$ctx = stream_context_create();
+stream_context_set_option($ctx, 'ssl', 'local_cert', 'ck.pem');
+stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+// Open a connection to the APNS server
+$fp = stream_socket_client(
+	'ssl://gateway.sandbox.push.apple.com:2195', $err,
+	$errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+
+if (!$fp)
+	exit("Failed to connect: $err $errstr" . PHP_EOL);
+
+echo 'Connected to APNS' . PHP_EOL;
+
+// Create the payload body
+$body['aps'] = array(
+	'alert' => $message,
+	'sound' => 'default',
+	'badge' => 1000
+	);
+$body['id'] = 1;
+
+// Encode the payload as JSON
+$payload = json_encode($body);
+
+// Build the binary notification
+$msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+
+// Send it to the server
+$result = fwrite($fp, $msg, strlen($msg));
+
+if (!$result)
+	echo 'Message not delivered' . PHP_EOL;
+else
+	echo 'Message successfully delivered' . PHP_EOL;
+
+// Close the connection to the server
+fclose($fp);
